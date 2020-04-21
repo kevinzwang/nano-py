2f5cd1b982e75dd8a0f5c6f39a220ff617f46db2
Kevin Wang <20215378+kevinzwang@users.noreply.github.com>
2020-04-21 00:44:23 -0700

    hopefully fix disconnect closing issue

--- bot.py
+++ bot.py
@@ -32,13 +32,14 @@ class Nano(commands.Bot):
         self.command_prefix = (f'<@!{self.user.id}> ', f'<@{self.user.id}> ')
         await self.change_presence(activity=discord.Game('type `help!`'))
 
-        if hasattr(self, 'http_session'):
-            await self.http_session.close()
-        self.http_session = aiohttp.ClientSession()
-
         print('Nano is connected!')
 
+    async def on_connect(self):
+        print('Connecting...')
+        self.http_session = aiohttp.ClientSession()
+
     async def on_disconnect(self):
+        print('Disconnecting...')
         await self.http_session.close()
 
     async def on_message(self, msg):
