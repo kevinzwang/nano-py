df95168be1a400188b0aa4fb1d8d69487345e918
Kevin Wang <20215378+kevinzwang@users.noreply.github.com>
2020-06-04 15:38:21 -0700

    add color to about command

--- cogs/meta.py
+++ cogs/meta.py
@@ -19,7 +19,8 @@ class Meta(commands.Cog):
 
         await ctx.send(embed=discord.Embed(
             title='Nano',
-            description='Cats, anime, music, Berkeley courses, and more.\nWritten in Python with love <3'
+            description='Cats, anime, music, Berkeley courses, and more.\nWritten in Python with love <3',
+            color=0x8C63D0
         ).add_field(
             name='Owner',
             value=app_info.owner.mention,
